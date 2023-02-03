674e8b1e8ff49f0780dadcb825cde184aa9ca432
arion5559 <42389714+arion5559@users.noreply.github.com>
2023-02-03 08:54:44 +0100

    Initial commit

---
+++ src/main/java/com/example/crudjavafx/LogIn.java
@@ -0,0 +1,17 @@
+package com.example.crudjavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LogIn {
+    @FXML
+    private TextField txtUsuario = new TextField();
+    @FXML
+    private PasswordField txtPsw = new PasswordField();
+    @FXML
+    private Button btnClick = new Button();
+
+}
