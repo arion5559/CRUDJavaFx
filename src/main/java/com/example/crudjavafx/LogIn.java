bb240f3df53f65ba541024a2fd9af7913d1a451f
arion5559 <42389714+arion5559@users.noreply.github.com>
2023-02-03 09:23:35 +0100

    Primer añadido

--- src/main/java/com/example/crudjavafx/LogIn.java
+++ src/main/java/com/example/crudjavafx/LogIn.java
@@ -1,17 +1,25 @@
 package com.example.crudjavafx;
 
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LogIn {
+    private Stage primaryStage;
+    private Scene scene;
+    private Parent root;
     @FXML
     private TextField txtUsuario = new TextField();
     @FXML
     private PasswordField txtPsw = new PasswordField();
     @FXML
     private Button btnClick = new Button();
+
+
 
 }
